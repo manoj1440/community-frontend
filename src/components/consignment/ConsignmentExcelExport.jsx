11d1d2feaf96299e83fd468ce79e91a2b40fd3e9
DeependraMB <deependramundalil2001@gmail.com>
2024-06-27 12:49:20 +0530

    Bug solved in ConsignmentExcelExport

--- src/components/consignment/ConsignmentExcelExport.jsx
+++ src/components/consignment/ConsignmentExcelExport.jsx
@@ -1,20 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Button, message } from 'antd';
 import * as XLSX from 'xlsx';
 import { FileExcelOutlined } from '@ant-design/icons';
 import { readableDate } from '../../utils/config';
 
 const ExcelExport = ({ data, buttonText, fileName, prices }) => {
-
-
+    
     const getRateForCommodity = (commodityName, warehouseName) => {
         const commodityPrice = prices.find((price) => {
             const priceCommodityName = price.commodityId?.name;
             const priceWarehouseName = price.warehouseId?.name;
-
             return priceCommodityName === commodityName && priceWarehouseName === warehouseName;
         });
-        return commodityPrice ? commodityPrice.historicalPrices.slice(-1)[0].price : 0; // Get the latest price
+        return commodityPrice ? commodityPrice.historicalPrices.slice(-1)[0].price : 0;
     };
 
     const handleExportExcel = () => {
@@ -26,17 +24,41 @@ const ExcelExport = ({ data, buttonText, fileName, prices }) => {
         const runningTotals = {};
 
         const tempRows = data.flatMap((r) => {
-            const commodityDetails = r.commodity.map((c) => ({
-                commodityName: c.commodityId ? c.commodityId.name : '',
-                noOfBags: c.bags ? c.bags[0].noOfBags : 0,
-                weight: c.bags ? c.bags[0].weight : 0,
-                quantity: c.bags ? c.bags[0].quantity : 0,
-                amount: c.amount || 0,
-                warehouseName: r.warehouseId ? r.warehouseId.name : ''
-            }));
+            const commodityDetails = r.commodity.map((c) => {
+                let totalBags = 0;
+                let totalWeight = 0;
+                let totalQuantity = 0;
+                let singleTotalBags = 0;
+                let singleTotalWeight = 0;
+                let singleTotalQuantity = 0;
+
+                if (c.bags && c.bags.length > 1) {
+                    c.bags.forEach(bag => {
+                        totalBags += bag.noOfBags;
+                        totalWeight += bag.weight;
+                        totalQuantity += bag.quantity;
+                    });
+                } else {
+                    singleTotalBags = c.bags ? c.bags[0].noOfBags : 0;
+                    singleTotalWeight = c.bags ? c.bags[0].weight : 0;
+                    singleTotalQuantity = c.bags ? c.bags[0].quantity : 0;
+                }
+
+                return {
+                    commodityName: c.commodityId ? c.commodityId.name : '',
+                    totalBags,
+                    totalWeight,
+                    totalQuantity,
+                    singleTotalBags,
+                    singleTotalWeight,
+                    singleTotalQuantity,
+                    amount: c.amount || 0,
+                    warehouseName: r.warehouseId ? r.warehouseId.name : ''
+                };
+            });
 
             return commodityDetails.map((detail) => {
-                const { commodityName, noOfBags, weight, amount, warehouseName } = detail;
+                const { commodityName, totalBags, totalWeight, totalQuantity, amount, warehouseName, singleTotalBags, singleTotalWeight, singleTotalQuantity } = detail;
 
                 if (!runningTotals[commodityName]) {
                     runningTotals[commodityName] = { bags: 0, kgs: 0 };
@@ -45,10 +67,8 @@ const ExcelExport = ({ data, buttonText, fileName, prices }) => {
                 const openingStockBags = runningTotals[commodityName].bags;
                 const openingStockKgs = runningTotals[commodityName].kgs;
 
-                runningTotals[commodityName].bags += noOfBags;
-                runningTotals[commodityName].kgs += weight;
-
-                const totalWeight = noOfBags * weight;
+                runningTotals[commodityName].bags += totalBags;
+                runningTotals[commodityName].kgs += totalWeight;
 
                 return {
                     "Date": readableDate(r.createdAt),
@@ -58,9 +78,9 @@ const ExcelExport = ({ data, buttonText, fileName, prices }) => {
                     "Item": commodityName,
                     'Opening Stock (Bags)': openingStockBags,
                     'Opening Stock (Kgs)': openingStockKgs,
-                    'No of Bags': noOfBags,
-                    'Avg Weight': weight,
-                    'Total Weight': totalWeight, 
+                    'No of Bags': singleTotalBags ? singleTotalBags : totalBags,
+                    'Avg Weight': singleTotalWeight ? singleTotalWeight : totalQuantity / totalBags,
+                    'Total Weight': singleTotalQuantity ? singleTotalQuantity : totalQuantity,
                     'Price Paid to Farmer': amount,
                     'Rate as per Calculation': getRateForCommodity(commodityName, warehouseName),
                     'Transferred': r.transferred,
@@ -68,7 +88,6 @@ const ExcelExport = ({ data, buttonText, fileName, prices }) => {
                 };
             });
         });
-
 
         const worksheet = XLSX.utils.json_to_sheet(tempRows);
         const workbook = XLSX.utils.book_new();
